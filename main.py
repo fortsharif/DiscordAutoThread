4e81f99f14ace60425d84251589226a0e85c0b52
fortsharif <huseinsharif.96@gmail.com>
2022-01-16 15:55:52 +0000

    UPDATED CODE FOR AUTOTHREAD BOT

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
 import discord 
 import os
+from discord.ext import commands
+
+
+# RE-WRITE ALL OF THIS LATER MESSY CODE.
 
 def main():
 
-    client = discord.Client()
+    global added_message 
+    added_message = ''
+
+    required_roles = ['Bot Devs', 'Fantasy Football Helpers', 'Admin', 'Mods']
+
+    client = commands.Bot(command_prefix='!' , intents = discord.Intents.all())
+    
     
 
     @client.event 
     async def on_message(message):
+        new_thread = None
+        global added_message
+        guild = client.get_guild(202794531488268289)
+        author_role = guild.get_member(int(message.author.id)).roles
+        user_roles = [role.name for role in author_role]
         text_channel = client.get_channel(int(os.environ.get('TEXT_CHANNEL')))
+        if any(x in required_roles for x in user_roles):
+            if message.content.startswith('!add'):
+                new_text = message.content.replace('!add','')
+                added_message = new_text
+                return await text_channel.send(content="*Added Message*")
+            if message.content.startswith('!remove'):
+                added_message = ''
+                return await text_channel.send(content="*Removed Message*")
+
+        
         if message.attachments and message.channel == text_channel:
-            await text_channel.create_thread(name = f'{message.author.name} RMT', message= message, auto_archive_duration = 60 )
+            new_thread = await text_channel.create_thread(name = f'{message.author.name} RMT', message=message, auto_archive_duration = 60 )
+            if len(added_message) > 0:
+                await new_thread.send(content=added_message)
+        
+
 
     client.run(os.environ.get("TOKEN"))
 
